e49ee0be9952911575c565101bf8d0723d0a5341
fivefh <lucaslangamessouza@gmail.com>
2023-02-25 15:05:46 -0400

    first commit

---
+++ javascript/inde.js
@@ -0,0 +1,14 @@
+let html = document.querySelector('html')
+let checkbox = document.querySelector('#dark')
+let deezer = document.querySelector('#deezer')
+
+checkbox.addEventListener('change', ()=>{
+  html.classList.add('dark-mode')
+    if(checkbox.checked){
+    let srcDark = deezer.getAttribute('src').replace('light', 'dark')
+    deezer.setAttribute('src', srcDark)
+  } else {
+    let srcLight = deezer.getAttribute('src').replace('dark', 'light')
+    deezer.setAttribute('src', srcLight)
+  }
+})
